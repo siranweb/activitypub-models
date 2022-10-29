f93f5deb2f117d6f629c9b47f1e594b39c822210
SiRanWeb <kirillg.web@gmail.com>
2022-10-29 21:05:36 +0300

    add ASObject description

--- src/models/asObject/ASObject.model.ts
+++ src/models/asObject/ASObject.model.ts
@@ -1,6 +1,13 @@
 import {ASBase} from "../ASBase.model";
 import { ASObjectFields } from "./ASObject.types";
 
+/**
+ * Describes an object of any kind. The Object type serves as the base type for most of
+ * the other kinds of objects defined in the Activity Vocabulary,
+ * including other Core types such as Activity, IntransitiveActivity, Collection and OrderedCollection.
+ *
+ * {@link https://www.w3.org/ns/activitystreams#Object Docs}
+ */
 export class ASObject extends ASBase<ASObjectFields>{
     constructor(fields: ASObjectFields) {
         super(fields);
